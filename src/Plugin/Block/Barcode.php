451e80d513c5296c6bf14eb4d3cc85b1a02a1e95
Stefan Auditor <stefan.auditor@erdfisch.de>
2016-09-08 12:26:48 +0200

    Issue #2797037 by sanduhrs: Value not defined in default block configuration

--- src/Plugin/Block/Barcode.php
+++ src/Plugin/Block/Barcode.php
@@ -71,6 +71,7 @@ class Barcode extends BlockBase implements ContainerFactoryPluginInterface {
   public function defaultConfiguration() {
     return [
       'type' => 'QRCODE',
+      'value' => '',
       'color' => '#000000',
       'height' => 100,
       'width' => 100,
